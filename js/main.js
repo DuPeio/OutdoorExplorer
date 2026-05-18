700acc1833cb61bce04a021426aba2a4f7be5ac8
magieup <magieup@univ-grenoble-alpes.fr>
2026-05-18 10:14:39 +0200

    Book animation working

--- js/main.js
+++ js/main.js
@@ -5,11 +5,10 @@ const number_of_sports = sports.length;
 const book = document.getElementById('book');
 
 function updateSportDisplay(idSport) {
-
     if (typeof sports !== 'undefined') {
         let sport = sports[idSport]
-        if (sport) {
 
+        if (sport) {
             // Tout les éléments nécessaires
             const sport_name = document.getElementById(`sport${idSport}`);
             const sport_name_container = document.getElementById(`container${idSport}`);
@@ -95,6 +94,7 @@ function updateSportDisplay(idSport) {
 
             if (material) {
                 material.innerHTML = sport.infos.materiel
+
             }
 
             if (cost) {
@@ -132,80 +132,16 @@ document.addEventListener('DOMContentLoaded', () => {
             rings.appendChild(ring);
         }
     }
-    updateSportDisplay(current_sport);
+
+    for(let i=1; i<number_of_sports+1; i++){
+        pageCreation(i);
+    }
+
+    for(let i=0; i<number_of_sports; i++){
+        updateSportDisplay(i);
+    }
 
 });
-
-let isOpen = false;
-
-//
-// //Couverture et 4eme de couverture
-// const cover = document.getElementById('cover');
-// cover.addEventListener('click', () => {
-//     if (current_page === 0) {
-//         cover.classList.add('flipping-forward');
-//         setTimeout(() => {
-//             current_page++;
-//             page.classList.remove('hidden');
-//         }, 150);
-//     } else if (current_page === 1) {
-//         cover.classList.remove('flipping-forward');
-//         setTimeout(() => {
-//             current_page--;
-//             page.classList.add('hidden');
-//         }, 350);
-//     }
-// });
-//
-// const lastCover = document.getElementById('lastCover');
-// lastCover.addEventListener('click', () => {
-//     console.log(current_page)
-//     if (current_page === number_of_sports + 1) {
-//         lastCover.classList.add('flipping-forward');
-//         setTimeout(() => {
-//             current_page++;
-//             page.classList.add('hidden');
-//             lastCover.style.zIndex = 1;
-//         }, 350);
-//     } else if (current_page === number_of_sports + 2) {
-//         setTimeout(() => {
-//             current_page--;
-//             page.classList.remove('hidden');
-//             lastCover.style.zIndex = -10;
-//         }, 150);
-//         lastCover.classList.remove('flipping-forward');
-//     } else {
-//         console.log(current_page)
-//         current_page++;
-//     }
-// });
-
-//Page de connexion TODO: Factoriser pour toutes les pages
-// const page1 = document.getElementById(`page${current_sport}`);
-// let right_page;
-// page1.addEventListener('click', (e) => {
-//     if (!is_connected) {
-//         alert("Connexion obligatoire pour aller à la page suivante.");
-//     } else if (is_connected && current_page === 1) {
-//         page1.classList.add('flipping-forward');
-//         right_page = pageCreation();
-//         console.log(right_page);
-//         right_page.classList.remove('hidden');
-//         updateSportDisplay(current_sport);
-//         setTimeout(() => {
-//             page.style.right = "35px";
-//             current_page++;
-//         }, 150)
-//     } else if (current_page === 2) {
-//         page1.classList.remove('flipping-forward');
-//         setTimeout(() => {
-//             page1.style.right = "23px";
-//             current_page--;
-//             book.removeChild(right_page);
-//         }, 350)
-//         updateSportDisplay(current_sport);
-//     }
-// })
 
 book.addEventListener('click', (e) => {
     const clickablePage = e.target.closest('.page, .cover');
@@ -221,70 +157,110 @@ function handlePageFlip(element) {
     const isFlipped = element.classList.contains('flipping-forward');
 
     if (!isFlipped) {
-        // Page de connexion
-        if (element.id === 'page0' && !is_connected) {
+    //     Cas particuliers (ne pas tourner)
+    //     4eme de couverture
+        if(element.id === 'lastCover' && current_page !== number_of_sports+1){
+            return;
+        }
+    //     Page de connexion
+        else if (element.id === 'page0' && !is_connected) {
             alert("Connexion obligatoire pour aller à la page suivante.");
             return;
         }
 
-        else if(element.id=== 'lastCover' && current_page !== number_of_sports+1){
-            return;
-        }
-
+        // Cas classiques (tourner l'élément)
         element.classList.add('flipping-forward');
 
-        setTimeout(() => {
-            if (element.id === 'cover' && current_page === 0) {
+
+
+    //     Cover
+        if(element.id === "cover") {
+            setTimeout(()=> {
                 document.getElementById('page0').classList.remove('hidden');
-            }
-            else if (element.id.startsWith('page')) {
-                element.style.right = "35px";
-                if(element.id === `page${number_of_sports}`) {
-                    return;
-                }
-                const right_page = pageCreation();
-                right_page.classList.remove('hidden');
+            },150)
 
+        }
 
-                updateSportDisplay(current_sport);
-                current_sport++;
-            }
-            else if (element.id === 'lastCover') {
-                const allPages = document.querySelectorAll('[id^="page"]');
-                allPages.forEach((page) => {
-                    page.classList.add('hidden');
-                })
-            }
+        // 4eme de cover
+        else if(element.id === "lastCover") {
+            setTimeout(() => {
+                document.getElementById(`page${number_of_sports}`).classList.add('hidden');
+                element.style.zIndex=1;
+            }, 350);
+        }
+
+        // Page de connexion
+        else if(element.id === 'page0') {
+
             current_page++;
-        }, 150);
+            setTimeout(() => {
+                element.style.right="35px";
+                document.getElementById('page1').classList.remove('hidden');
+            }, 150)
+        }
 
-    } else {
-        if(element.id ==="cover" && current_page !== 1) {
+    //     Pages
+        else {
+            current_page++;
+            setTimeout(() => {
+                element.style.right="35px";
+                document.getElementById(`page${current_page - 2}`).classList.add('hidden');
+
+            }, 350);
+
+            if(current_page <= number_of_sports){
+                setTimeout(() => {
+                    document.getElementById(`page${current_page}`).classList.remove('hidden');
+                }, 150);
+            }
+
+        }
+    }
+
+    else{
+        // Cas particuliers (ne pas tourner)
+        // Cover
+        if(element.id === 'cover' && current_page !== 0){
             return;
         }
+
+        // Cas classiques (on peut tourner)
         element.classList.remove('flipping-forward');
-        // console.log(current_page);
-        setTimeout(() => {
-            if (element.id === 'cover') {
+
+        //     Couverture
+        if(element.id === 'cover') {
+            setTimeout(()=> {
                 document.getElementById('page0').classList.add('hidden');
-                current_page = 0;
-                return;
-            }
-            else if (element.id.startsWith('page')) {
-                element.style.right = "23px";
-                const pages = document.querySelectorAll('.page');
-                console.log(pages[current_sport].id, `page${number_of_sports}`)
-                if (pages.length > 1 && pages[current_sport].id === `page${number_of_sports-1}`) {
-                    book.removeChild(pages[pages.length - 1]);
-                }
-                current_sport--;
-            }
-            else if (element.id === 'lastCover') {
-                element.style.zIndex = -10;
-            }
+            }, 350)
+        }
+
+        //     4eme de couverture
+        else if (element.id === 'lastCover') {
+            setTimeout(() => {
+                document.getElementById(`page${number_of_sports}`).classList.remove('hidden');
+                element.style.zIndex=-5;
+            },150)
+        }
+        //     Pages
+        else{
+
             current_page--;
-            updateSportDisplay(current_sport);
-        }, 350);
+            if(current_page+1 <= number_of_sports){
+                setTimeout(() => {
+                    document.getElementById(`page${current_page+1}`).classList.add('hidden');
+
+                },350)
+            }
+
+            if(current_page >= 1){
+
+                setTimeout(()=> {
+                    element.style.right="23px";
+                    document.getElementById(`page${current_page-1}`).classList.remove('hidden');
+                }, 150)
+            }
+
+        }
     }
 }
 
@@ -294,11 +270,12 @@ form.addEventListener('click', (e) => {
 });
 
 // Création de la page
-function pageCreation() {
+function pageCreation(id_page) {
+    let id_sport = id_page-1;
     let newPage = document.createElement("div");
     newPage.classList.add('page');
     newPage.classList.add('hidden');
-    newPage.id = `page${current_sport+1}`; // TODO : Attention au numéro de la page
+    newPage.id = `page${id_page}`; // TODO : Attention au numéro de la page
 
     // Front
     let newFront = document.createElement('div');
@@ -308,7 +285,7 @@ function pageCreation() {
 
     let newGameDone = document.createElement('div');
     newGameDone.classList.add('game-not-done');
-    newGameDone.id = `game-not-done${current_sport}`;
+    newGameDone.id = `game-not-done${id_sport}`;
     newGameDone.innerHTML = "Déverrouillez les informations en jouant au mini-jeu !";
     newFront.appendChild(newGameDone);
 
@@ -318,7 +295,7 @@ function pageCreation() {
 
     let newConfidential = document.createElement('div');
     newConfidential.classList.add('confidential');
-    newConfidential.id = `confidential${current_sport}`;
+    newConfidential.id = `confidential${id_sport}`;
     newInfoContainer.appendChild(newConfidential);
 
     let newInfo = document.createElement('div');
@@ -327,12 +304,12 @@ function pageCreation() {
 
     let newResume = document.createElement('div');
     newResume.classList.add('resume');
-    newResume.id = `resume${current_sport}`;
+    newResume.id = `resume${id_sport}`;
     newInfo.appendChild(newResume);
 
     let newSeason = document.createElement('div');
     newSeason.classList.add('season');
-    newSeason.id = `season${current_sport}`;
+    newSeason.id = `season${id_sport}`;
     newInfo.appendChild(newSeason);
 
     let newH4Cost = document.createElement('h4');
@@ -341,7 +318,7 @@ function pageCreation() {
 
     let newCost = document.createElement('div');
     newCost.classList.add('cost');
-    newCost.id = `cost${current_sport}`;
+    newCost.id = `cost${id_sport}`;
     newInfo.appendChild(newCost);
 
     let newMaterialBenefits = document.createElement('div');
@@ -358,7 +335,7 @@ function pageCreation() {
 
     let newMaterial = document.createElement('div');
     newMaterial.classList.add('material');
-    newMaterial.id = `material${current_sport}`;
+    newMaterial.id = `material${id_sport}`;
     newMaterialContainer.appendChild(newMaterial);
 
     let newBenefitsContainer = document.createElement('div');
@@ -371,7 +348,7 @@ function pageCreation() {
 
     let newBenefits = document.createElement('div');
     newBenefits.classList.add('benefits');
-    newBenefits.id = `benefits${current_sport}`;
+    newBenefits.id = `benefits${id_sport}`;
     newBenefitsContainer.appendChild(newBenefits);
 
     let newH4Tips = document.createElement('h4');
@@ -380,12 +357,12 @@ function pageCreation() {
 
     let newTips = document.createElement('div');
     newTips.classList.add('tips');
-    newTips.id = `tips${current_sport}`;
+    newTips.id = `tips${id_sport}`;
     newInfo.appendChild(newTips);
 
     let newClubLink = document.createElement('div');
     newClubLink.classList.add('club-link');
-    newClubLink.id = `club${current_sport}`;
+    newClubLink.id = `club${id_sport}`;
 
     // Back
         let newBack = document.createElement('div');
@@ -393,83 +370,39 @@ function pageCreation() {
         newBack.classList.add('face');
         newPage.appendChild(newBack);
 
-    if (current_sport < number_of_sports-1){
+    if (id_sport < number_of_sports-1){
         let newContainer = document.createElement('div');
         newContainer.classList.add('sport-container');
         newBack.appendChild(newContainer);
 
         let newSportNameContainer = document.createElement('div');
         newSportNameContainer.classList.add('sport-name-container');
-        newSportNameContainer.id = `container${current_sport+1}`;
+        newSportNameContainer.id = `container${id_sport+1}`;
         newContainer.appendChild(newSportNameContainer);
 
         let newSportName = document.createElement('div');
         newSportName.classList.add('sport-name');
-        newSportName.id = `sport${current_sport+1}`;
+        newSportName.id = `sport${id_sport+1}`;
         newSportNameContainer.appendChild(newSportName);
 
         let newDescription = document.createElement('div');
         newDescription.classList.add('description');
-        newDescription.id = `description${current_sport+1}`;
+        newDescription.id = `description${id_sport+1}`;
         newContainer.appendChild(newDescription);
 
         let newBadge = document.createElement('img');
         newBadge.classList.add('badge');
-        newBadge.id = `badge${current_sport+1}`;
+        newBadge.id = `badge${id_sport+1}`;
         newContainer.appendChild(newBadge);
 
         let newButton = document.createElement('button');
         newButton.classList.add('game-btn');
-        newButton.id = `game${current_sport+1}`;
+        newButton.id = `game${id_sport+1}`;
         newButton.innerHTML = "Lancer le mini jeu"
         newContainer.appendChild(newButton);
     }
 
-
     book.appendChild(newPage);
 
     return newPage;
-
 }
-
-
-
-// // <div className="page-wrapper right hidden" id="rightPage">-->
-// <!--                <div class="page-face front right-page sport-page ">-->
-// <!--                    <div class="game-not-done" id="game-not-done">Déverrouillez les informations en jouant au mini-jeu !</div>-->
-//
-// <!--                    <div class="informations-container">-->
-// <!--                        <div class="confidential hidden" id="confidential"></div>-->
-// <!--                        <div class="informations">-->
-// <!--                            <div class="resume" id = "resume"></div>-->
-//
-// <!--                            <div class="season" id="season"></div>-->
-//
-// <!--                            <h4>Coût de la pratique</h4>-->
-// <!--                            <div class="cost" id="cost"></div>-->
-//
-// <!--                            <div class="material-benefits">-->
-// <!--                                <div class="material-container">-->
-// <!--                                    <h4>Matériel nécessaire</h4>-->
-// <!--                                    <div class="material" id="material"></div>-->
-// <!--                                </div>-->
-//
-// <!--                                <div class="benefits-container">-->
-// <!--                                    <h4>Bienfaits</h4>-->
-// <!--                                    <div class="benefits" id="benefits"></div>-->
-// <!--                                </div>-->
-// <!--                            </div>-->
-//
-//
-// <!--                            <h4>Conseils</h4>-->
-// <!--                            <div class="tips" id = "tips"></div>-->
-//
-// <!--                            <div class="club-link" id="club">Si tu veux plus <b>d'informations</b> pour pratiquer rends-toi sur ce(s) lien(s) : </div>-->
-//
-// <!--                        </div>-->
-//
-// <!--                    </div>-->
-// <!--                </div>-->
-// <!--            </div>-->
-
-
